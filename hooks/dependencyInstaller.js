4b1184f10aa562e8bbabd46ab4a396b455d0a02d
_Louis_ <hmtlouis@gmail.com>
2021-01-08 12:32:07 +0800

    2.0.12

--- hooks/dependencyInstaller.js
+++ hooks/dependencyInstaller.js
@@ -1,15 +1,7 @@
-console.log("Running hook to install hooks pre-requisites");
+console.log("Running hook to npm install hooks pre-requisites");
 module.exports = function (context) {
-  return new Promise((resolve, reject) => {
-    var child_process = require('child_process');
-    child_process.exec('npm install', {cwd: __dirname}, function (error) {
-      if (error !== null) {
-        console.log('exec error: ' + error);
-        reject('npm installation failed');
-      }
-      else {
-        resolve();
-      }
-    });
-  });
+  var shell = context.requireCordovaModule('shelljs');
+
+  shell.cd(context.opts.plugin.dir);
+  shell.exec('npm install');
 };
